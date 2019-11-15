04a70553cb0bb1aab27a22a85eecca3c801b23b3
PK André <pululuandre@hotmail.com>
2019-11-15 15:01:33 +0100

    Add Lang and Shop
    Alter log tab add lang et shop in upgrade process

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -2,5 +2,6 @@ SET SESSION sql_mode = '';
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
+ALTER TABLE `PREFIX_log` ADD `id_shop` INT(10) NULL DEFAULT NULL AFTER `object_id`, ADD `id_lang` INT(10) NULL DEFAULT NULL AFTER `id_shop`;
 
 /* PHP:ps_1770_preset_tab_enabled(); */;
