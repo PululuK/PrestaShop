6196889d38d3740fd74bb1ac791cf7777115abe9
PK André <pululuandre@hotmail.com>
2019-11-15 16:20:44 +0100

    Add lang and shop fields to log table

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -2,6 +2,28 @@ SET SESSION sql_mode = '';
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
-ALTER TABLE `PREFIX_log` ADD `id_shop` INT(10) NULL DEFAULT NULL AFTER `object_id`, ADD `id_lang` INT(10) NULL DEFAULT NULL AFTER `id_shop`;
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_DISPLAY_MANUFACTURERS', '1', NOW(), NOW());
 
 /* PHP:ps_1770_preset_tab_enabled(); */;
+
+/* Add field MPN to tables */
+ALTER TABLE `PREFIX_order_detail` ADD `product_mpn` VARCHAR(40) NULL AFTER `product_upc`;
+ALTER TABLE `PREFIX_supply_order_detail` ADD `mpn` VARCHAR(40) NULL AFTER `upc`;
+ALTER TABLE `PREFIX_stock` ADD `mpn` VARCHAR(40) NULL AFTER `upc`;
+ALTER TABLE `PREFIX_product_attribute` ADD `mpn` VARCHAR(40) NULL AFTER `upc`;
+ALTER TABLE `PREFIX_product` ADD `mpn` VARCHAR(40) NULL AFTER `upc`;
+
+/* Delete price display precision configuration */
+DELETE FROM `PREFIX_configuration` WHERE name = 'PS_PRICE_DISPLAY_PRECISION';
+
+/* Set optin field value to 0 in employee table */
+ALTER TABLE `PREFIX_employee` MODIFY COLUMN `optin` tinyint(1) unsigned DEFAULT NULL;
+
+/* Increase column size */
+ALTER TABLE `PREFIX_hook` CHANGE `name` `name` VARCHAR(191) NOT NULL;
+ALTER TABLE `PREFIX_hook` CHANGE `title` `title` VARCHAR(255) NOT NULL;
+ALTER TABLE `PREFIX_hook_alias` CHANGE `name` `name` VARCHAR(191) NOT NULL;
+ALTER TABLE `PREFIX_hook_alias` CHANGE `alias` `alias` VARCHAR(191) NOT NULL;
+
+/* Add lang and shop fields to log table */
+ALTER TABLE `PREFIX_log` ADD `id_shop` INT(10) NULL DEFAULT NULL AFTER `object_id`, ADD `id_lang` INT(10) NULL DEFAULT NULL AFTER `id_shop`;
